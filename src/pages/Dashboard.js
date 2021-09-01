7749426e4f6f3d06022fdb6e5f049a3d03ce657a
Neil M <n.mckenzie@live.com>
2021-09-01 12:04:46 -0500

    first commit of basic app

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Info, Repos, User, Search, Navbar } from "../components";
+import loadingImage from "../images/preloader.gif";
+import { GithubContext } from "../context/context";
+const Dashboard = () => {
+  return (
+    <main>
+      <Search></Search>
+      <Info></Info>
+      <User></User>
+      <Repos></Repos>
+    </main>
+  );
+};
+
+export default Dashboard;
