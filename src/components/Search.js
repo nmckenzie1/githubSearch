89046bd484c1aa2d6153414f150baa5ae3c96e39
Neil M <n.mckenzie@live.com>
2021-09-01 13:09:33 -0500

    added some get requests for user data

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,13 +4,25 @@ import { MdSearch } from "react-icons/md";
 import { GithubContext } from "../context/context";
 const Search = () => {
   const [user, setUser] = React.useState("");
+  const { requests, error, searchGithubUser, isLoading } =
+    React.useContext(GithubContext);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(user);
+    if (user) {
+      searchGithubUser(user);
+      setUser("");
+    }
   };
   return (
     <section className="section">
       <Wrapper className="section-center">
+        {error.show && (
+          <ErrorWrapper>
+            <p>{error.msg}</p>
+          </ErrorWrapper>
+        )}
         <form onSubmit={handleSubmit}>
           <div className="form-control">
             <MdSearch />
@@ -20,10 +32,12 @@ const Search = () => {
               value={user}
               onChange={(e) => setUser(e.target.value)}
             />
-            <button type="submit">search</button>
+            {requests > 0 && !isLoading && (
+              <button type="submit">search</button>
+            )}
           </div>
         </form>
-        <h3>requests : 60 / 60</h3>
+        <h3>requests : {requests} / 60</h3>
       </Wrapper>
     </section>
   );
--- src/context/context.js
+++ src/context/context.js
@@ -12,8 +12,60 @@ const GithubProvider = ({ children }) => {
   const [githubUser, setGithubUser] = useState(mockUser);
   const [repos, setRepos] = useState(mockRepos);
   const [followers, setFollowers] = useState(mockFollowers);
+  const [requests, setRequests] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState({ show: false, msg: "" });
+
+  const searchGithubUser = async (user) => {
+    toggleError();
+    setIsLoading(true);
+    const response = await axios(`${rootUrl}/users/${user}`).catch((err) =>
+      console.log(err)
+    );
+    if (response) {
+      setGithubUser(response.data);
+      const { login, followers_url } = response.data;
+      axios(`${rootUrl}/users/${login}/repos?per_page=100`).then((response) => {
+        setRepos(response.data);
+      });
+      axios(`${followers_url}?per_page=100`).then((response) => {
+        setFollowers(response.data);
+      });
+    } else {
+      toggleError(true, "user doesn't exist");
+    }
+    checkRequest();
+    setIsLoading(false);
+  };
+
+  const checkRequest = () => {
+    axios(`${rootUrl}/rate_limit`)
+      .then(({ data }) => {
+        setRequests(data.rate.remaining);
+        if (data.rate.remaining === 0) {
+          toggleError(true, "Rate limit reached. Try again in an hour!");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  function toggleError(show, msg) {
+    setError({ show, msg });
+  }
+  useEffect(checkRequest, []);
   return (
-    <GithubContext.Provider value={{ githubUser, repos, followers }}>
+    <GithubContext.Provider
+      value={{
+        error,
+        requests,
+        githubUser,
+        repos,
+        followers,
+        searchGithubUser,
+        isLoading,
+      }}
+    >
       {children}
     </GithubContext.Provider>
   );
